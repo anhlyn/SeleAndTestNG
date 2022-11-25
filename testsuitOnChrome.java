fc761e7f3aa7f9340373d95153fe632aec23c1f0
linhtran <lyn.tran.anh@gmail.com>
2022-11-25 21:08:17 +0700

    Innitialize launch chrome driver.

--- testsuitOnChrome.java
+++ testsuitOnChrome.java
@@ -1,15 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
 
 public class testsuitOnChrome {
 
     @Test
     public void TC_1(){
         System.out.println("Exec TC_1.");
-    }
+        System.setProperty("webdriver.chrome.driver","//Users//nghitran//Linh//SeleAndTestNG//BrowserDrivers//chromedriver");
+        WebDriver driver = new ChromeDriver();
 
-    @Test
-    public void TC_2(){
-        System.out.println("Exec TC_2.");
+        driver.manage().window().maximize();
+        driver.get("https://www.geeksforgeeks.org/");
     }
 
 }
