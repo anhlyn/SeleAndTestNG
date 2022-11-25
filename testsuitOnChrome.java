307ace80a779d5fadf1557032f8cca44f8047c2c
linhtran <lyn.tran.anh@gmail.com>
2022-11-25 19:39:24 +0700

    Add example file and .gitignore

---
+++ testsuitOnChrome.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class testsuitOnChrome {
+
+    @Test
+    public void TC_1(){
+        System.out.println("Exec TC_1.");
+    }
+
+    @Test
+    public void TC_2(){
+        System.out.println("Exec TC_2.");
+    }
+
+}
